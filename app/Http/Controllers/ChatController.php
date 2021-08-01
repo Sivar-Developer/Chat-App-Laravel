c7ced949b1ad9e13a5a411f7c906ffefd3700bf0
Sivar Sarkawt <fragmatica.dc@gmail.com>
2021-08-01 11:39:51 +0300

    Scaffold authentication breeze

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -3,8 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\ChatConversation;
+use App\Models\ChatParticipant;
+use App\Models\ChatMessage;
 
 class ChatController extends Controller
 {
-    //
+    public function conversaions()
+    {
+        $conversations = auth('api')->user()->participants()->conversation()->with('messages', function($query) {
+            return $query->latest()->first();
+        })->get();
+
+        return response()->json($conversations, 200);
+    }
+
+    public function storeMessage(Request $request)
+    {
+
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return response()->json([
-        'Module' => 'Chat App',
-        'Version' => '1.0.0',
-        'Credit' => 'Sivar Sarkawt'
-    ], 200);
+    return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
